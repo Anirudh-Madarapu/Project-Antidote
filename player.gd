ff4f86c00091fa93d3d9c0e8102a6f962f41e34b
Anirudh Madarapu <123264579+Anirudh-Madarapu@users.noreply.github.com>
2025-01-30 11:27:15 -0600

    Added upward movement

--- player.gd
+++ player.gd
@@ -15,7 +15,8 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	
+	if Input.is_action_just_pressed("ui_up"):
+		velocity.y += 1.0
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
