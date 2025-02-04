9ffb669b99c3ee9f229a18215fdb762c1710a049
Anirudh Madarapu <123264579+Anirudh-Madarapu@users.noreply.github.com>
2025-02-04 11:39:27 -0600

    Player moment added for Y-axis

--- player.gd
+++ player.gd
@@ -11,8 +11,8 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	#if not is_on_floor():
+	#	velocity.y += gravity * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_up"):
@@ -23,8 +23,11 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction_y = Input.get_axis("ui_down","ui_up")
 	if direction:
 		velocity.x = direction * SPEED
+	if direction_y:
+		velocity.y = -1* direction_y * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
