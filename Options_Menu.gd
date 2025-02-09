5f1afa675710b98a3308b8ac8f4b646aca4a2f56
I-AmTamiaWare <tmware@knox.edu>
2025-02-08 18:26:12 -0600

    options menu

---
+++ Options_Menu.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+
+
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://start_menu.tscn")
+
+
+func _on_music_sfx_pressed():
+	pass # Replace with function body.
+
+
+func _on_controls_pressed():
+	pass # Replace with function body.
