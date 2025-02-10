87f479fb79a1aacb4da4673f3eb0361447bfa588
Kurooco <128552915+Kurooco@users.noreply.github.com>
2025-02-09 19:21:30 -0600

    created items
    - interaction areas created
    - conversation areas created

---
+++ InteractionArea.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+
+signal interaction_initiated
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if(Input.is_action_just_pressed("interact") && get_overlapping_areas().size() > 0):
+		interaction_initiated.emit()
