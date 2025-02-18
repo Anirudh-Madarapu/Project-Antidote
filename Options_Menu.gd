f1643c4039aa1f265d2245bfb51f15d538eeb7b0
I-AmTamiaWare <tmware@knox.edu>
2025-02-17 18:34:47 -0600

    finished start menu

--- Options_Menu.gd
+++ Options_Menu.gd
@@ -1,14 +1,17 @@
-extends Node2D
+extends Control
+
+
+
+func _on_controls_pressed():
+	print("Controls pressed")
+
+
+
+func _on_music_sfx_pressed():
+	print("Music/SFX pressed")
 
 
 
 func _on_back_pressed():
-	get_tree().change_scene_to_file("res://start_menu.tscn")
-
-
-func _on_music_sfx_pressed():
-	pass # Replace with function body.
-
-
-func _on_controls_pressed():
-	pass # Replace with function body.
+	print("Back pressed")
+	get_tree().change_scene_to_file("res://start_menu_.tscn")
