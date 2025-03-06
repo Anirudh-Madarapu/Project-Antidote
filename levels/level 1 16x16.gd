5449a76cc50a3b757b397a66bd725caee0085090
Daphne Ritz <daphneritz@outlook.com>
2025-03-06 11:27:31 -0600

    Revert "Merge branch 'development' into Daphne"
    This reverts commit 1e8d3395b45a7615fe454420624e6ef2e51fe451, reversing
    changes made to d8e1353fd0f29e3069df2d2bf62ad6ee8d78eaf1.

--- levels/level 1 16x16.gd
+++ levels/level 1 16x16.gd
@@ -6,7 +6,7 @@ extends Node2D
 func _ready():
 	$first_door.close()
 	$"/root/Autoload".dialogue_handler.conversation_ended.connect(open_door)
-	$"/root/Autoload".dialogue_handler.start_conversation(beginning_conv)
+	#$"/root/Autoload".dialogue_handler.start_conversation(beginning_conv)
 	$"/root/Autoload".level = 1
 	$"/root/Autoload".health = 100
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- player.gd
+++ player.gd
@@ -13,19 +13,14 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var anim = $AnimatedSprite2D
 
 @onready var health_bar = $CanvasLayer/ProgressBar
-@onready var serum_bar = $CanvasLayer2/serumbar
 #@onready var armor_bar = $healthbar
-var serum_bar_value = 60
+
 var attack_in_range = false
 
 func handle_attack():
 	if Input.is_action_just_pressed("attack"):  # Make sure to set up "attack" action as "A" key in project settings
 		is_attacking = true
 		anim.play("attack")  # Play attack animation
-		serum_bar.value -= 20
-		serum_bar_value -= 20
-		if serum_bar_value < 60:
-			$Timer.start()
 		#$"attack noise".play()		
 		await anim.animation_finished  # Wait for attack animation to finish
 		is_attacking = false
@@ -37,23 +32,6 @@ func _ready():
 	if health <= 20:
 		anim.play("idle2")
 
-	
-
-func _on_timer_timeout():
-	serum_bar.value += 20
-	serum_bar_value +=20
-	
-	# Ensure the serum bar does not exceed the maximum value
-	if serum_bar.value > 60:
-		serum_bar.value = 60
-		serum_bar_value = 60
-		$Timer.stop()
-	
-	# Stop the timer if the serum bar is full
-	if serum_bar.value == 60:
-		$Timer.stop() #Replace with function body.
-	
-	
 func _physics_process(delta):
 	# Handle attack first
 	if Input.is_action_just_pressed("attack"):
@@ -134,7 +112,6 @@ func simulate_damage():
 		if attack_in_range:
 			health -= 10  # Subtract 10 from health
 			update_health()  # Update the health bar
-
 			print("Health after iteration ", i + 1, ": ", health)  # Print current health
 			await get_tree().create_timer(1.0).timeout  # Wait for 1 second (optional delay)
 		
@@ -156,6 +133,3 @@ func attack():
 	if attack_in_range:
 		
 		print('')
-
-
-
