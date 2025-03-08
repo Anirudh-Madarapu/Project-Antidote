3aeb2fa14af7073730a1a7b93399dd0d4e2f7867
Kurooco <128552915+Kurooco@users.noreply.github.com>
2025-03-08 13:49:03 -0600

    added rocket, npc, and effects to final room

---
+++ scripts/end_room.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$"/root/Autoload".dialogue_handler.conversation_ended.connect(put_together)
+	$"/root/Autoload".dialogue_handler.start_conversation(11)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+func put_together(c):
+	if(c == 11):
+		$PutTheRocketTogether.start()
