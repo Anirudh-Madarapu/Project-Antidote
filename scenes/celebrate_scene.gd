fc2b0947caff29155601e5a80d782f209beec7b7
Anirudh Madarapu <123264579+Anirudh-Madarapu@users.noreply.github.com>
2025-03-10 16:40:41 -0500

    Adding a celebrate scene

---
+++ scenes/celebrate_scene.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+	$"level 2 16x16/zombie1".change()
+	$"level 2 16x16/Zombie2".change()
+	$"level 2 16x16/Zombie3".change()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
