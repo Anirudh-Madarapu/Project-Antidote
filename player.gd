94eb9605192bec70b27278fe02deb66fe1126ff2
Anirudh Madarapu <123264579+Anirudh-Madarapu@users.noreply.github.com>
2025-03-12 20:09:15 -0500

    Lowered Zombie and Player speed

--- player.gd
+++ player.gd
@@ -84,13 +84,13 @@ func _physics_process(delta):
 	
 	# Handle horizontal movement
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * SPEED * 0.6
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		
 	# Handle vertical movement
 	if direction_y:
-		velocity.y = -direction_y * SPEED
+		velocity.y = -direction_y * SPEED *0.6
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 	
